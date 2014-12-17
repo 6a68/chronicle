fa12d8cfde2fc109a970c1647c9b787b8d598f29
Jared Hirsch <ohai@6a68.net>
2014-12-16 17:07:46 -0800

    Squash ALL THE THINGS!!!
    Retaining the 21 original commit messages for now just in case I need em:
    
    add buf, for handling binary uuid ids, and also mozlog
    
    wrap mozlog init in a lib and use in the main server
    
    connect and disconnect from mysql
    
    add very stupid example configs for oauth. TODO: use something better
    
    drop db, then re-create it and create user table
    
    put some fake data in the db
    
    WIP auth-related hapi commit
    
    extract + continue configuring routes
    
    auth routes shouldn't be api routes: they redirect the whole page
    
    fix little bugs, now the server starts yay
    
    doh strict mode
    
    shore up the hapi bell configs
    
    correctly configure auth check to avoid infinite redirect loop
    
    add dummy front-end code to be replaced
    
    super ugly / barely works...but works
    
    split out db creation code; export db function to persist user
    
    clean up routes file a bit
    
    add user lookup call to db; switch from connection to pool
    
    update deps, npm-shrinkwrap
    
    actually grab a conn from the pool
    
    Save the user record in the DB after profile fetch
    
    Also now correctly storing oauth_token as a varchar field, not a
    fixed-length binary field.

--- server/config.js
+++ server/config.js
@@ -2,10 +2,13 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+'use strict';
+
 var fs = require('fs');
 var path = require('path');
 
 var convict = require('convict');
+var SEVEN_DAYS_IN_MSEC = 1000 * 60 * 60 * 24 * 7;
 
 var conf = convict({
   env: {
@@ -41,6 +44,16 @@ var conf = convict({
       password: {
         default: '',
         env: 'MYSQL_PASSWORD'
+      },
+      database: {
+        doc: 'Name of the database.',
+        default: 'chronicle',
+        env: 'MYSQL_DATABASE'
+      },
+      connectionLimit: {
+        doc: 'Max number of MySQL workers in the pool.',
+        default: 25,
+        env: 'MYSQL_CONN_LIMIT'
       }
     },
     redis: {
@@ -87,8 +100,67 @@ var conf = convict({
     },
     staticPath: {
       doc: 'Path to static files.',
-      default: 'app',
+      default: 'dist',
       env: 'STATIC_PATH'
+    },
+    staticDirListing: {
+      doc: 'Whether to allow directory listings for static dirs.',
+      default: false,
+      env: 'STATIC_DIR_LISTING'
+    },
+    oauth: {
+      clientId: {
+        doc: 'Firefox Accounts client_id.',
+        default: '5901bd09376fadaa',
+        env: 'FXA_OAUTH_CLIENT_ID'
+      },
+      clientSecret: {
+        doc: 'Firefox Accounts OAuth client_secret.',
+        default: '4ab433e31ef3a7cf7c20590f047987922b5c9ceb1faff56f0f8164df053dd94c',
+        env: 'FXA_OAUTH_CLIENT_SECRET'
+      },
+      scope: {
+        doc: 'Application scope. Currently not checked by the auth server.',
+        default: ['chronicle'],
+        env: 'FXA_OAUTH_SCOPE'
+      },
+      version: {
+        doc: 'OAuth version.',
+        default: '2.0',
+        env: 'FXA_OAUTH_VERSION'
+      },
+      protocol: {
+        doc: 'OAuth authorization protocol used.',
+        default: 'oauth2',
+        env: 'FXA_OAUTH_AUTH_PROTOCOL'
+      },
+      authEndpoint: {
+        doc: 'Firefox Accounts OAuth authorization endpoint.',
+        default: 'https://oauth-latest.dev.lcip.org/v1/authorization',
+        env: 'FXA_OAUTH_AUTH_ENDPOINT'
+      },
+      tokenEndpoint: {
+        doc: 'Firefox Accounts OAuth token endpoint.',
+        default: 'https://oauth-latest.dev.lcip.org/v1/token',
+        env: 'FXA_OAUTH_TOKEN_ENDPOINT'
+      }
+    },
+    session: {
+      password: {
+        doc: 'Password used to encrypt cookies with hueniverse-iron.',
+        default: 'S3kr1t',
+        env: 'SESSION_PASSWORD'
+      },
+      isSecure: {
+        doc: 'Whether to allow cookie to be transmitted over insecure connections.',
+        default: false,
+        env: 'ALLOW_HTTP_COOKIES'
+      },
+      duration: {
+        doc: 'Default session TTL for authenticated users.',
+        default: SEVEN_DAYS_IN_MSEC,
+        env: 'SESSION_DURATION'
+      }
     }
   }
 });
--- server/index.js
+++ server/index.js
@@ -5,22 +5,66 @@
 'use strict';
 
 var Hapi = require('hapi');
-var server = new Hapi.Server();
-var config = require('./config').root();
+var path = require('path');
+var config = require('./config');
+var log = require('./logger')('server.index');
+var routes = require('./routes');
+
+var server = new Hapi.Server({ debug: { request: ['error'] } });
 server.connection({
-  host: config.server.host,
-  port: config.server.port
+  host: config.get('server.host'),
+  port: config.get('server.port')
 });
-server.route({
-  method: 'GET',
-  path: '/{param*}',
-  handler: {
-    directory: {
-      path: config.server.staticPath,
-      listing: true
-    }
+
+server.register([require('hapi-auth-cookie'), require('bell')], function (err) {
+  if (err) {
+    log.warn('failed to load plugin: ' + err);
+    throw err; // TODO: should we use AppError instead?
   }
-});
-server.start(function () {
-  console.log('chronicle server running on port ' + config.server.port);
+
+  // hapi-auth-cookie init
+  // TODO: when we really have to, move this crap into a config file
+  server.auth.strategy('session', 'cookie', {
+    password: 'cookie_encryption_password',
+    cookie: 'sid-chronicle',
+    redirectTo: '/',
+    isSecure: false, 
+    ttl: 1000 * 60 * 60 * 24 * 7
+  });
+
+  // bell init
+  // TODO move into a config file only when we actually have to 
+  server.auth.strategy('oauth', 'bell', {
+    provider: {
+      protocol: 'oauth2',
+      auth: 'https://oauth-latest.dev.lcip.org/v1/authorization',
+      token: 'https://oauth-latest.dev.lcip.org/v1/token',
+      version: '2.0',
+      scope: ['chronicle','profile'],
+      profile: function (credentials, params, get, profileCb) {
+        throw new Error('inside bell provider profile function');
+        log.info('inside the oauth-bell profile callback!');
+        log.info('credentials: ' + credentials);
+        log.info('params: ' + JSON.stringify(params));
+        log.info('get: ' + get);
+        log.info('profileCb: ' + profileCb);
+        // TODO here's a guess at what to do in here, bell provides no example:
+        // 1. grab params.token, send it to the profile endpoint
+        // 2. put the profile response into the credentials object, then send it to DB
+      }
+    },
+    password: 'cookie_encryption_password',
+    clientId: '1f9bbddcb3e160ab',
+    clientSecret: '24bf8caeaa685e2e42d9a75b48511f83adffaf6fcdd174ce8749358a376be911',
+    isSecure: false
+  });
+
+  server.route(routes);
+  server.start(function (err) {
+    if (err) {
+      log.warn('server failed to start: ' + err);
+      throw err; // TODO: should we fail to start in some other way? AppError?
+    }
+    log.info('chronicle server running on port ' + config.get('server.port'));
+  });
 });
